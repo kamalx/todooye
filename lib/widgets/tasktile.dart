f0efbfe0bb4f64e7a90eefa73c190eb1cfe5ab90
Kamal Morjal <kamal@morjal.com>
2020-11-21 08:05:04 +0530

    managing checkbox state
    - extract checkbox into a custom
    widget
    - lift state up one level, so that
    checkbox state can affect its
    siblings (other widgets at the
    same level)
    - conditionally style sibling widget
    based on checkbox state

--- lib/widgets/tasktile.dart
+++ lib/widgets/tasktile.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   final String title;
+
   TaskTile({
     @required this.title,
   });
 
   @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = true;
+
+  void checkboxCallback(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(title),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (v) {},
+      title: Text(
+        widget.title,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: checkboxCallback,
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+  TaskCheckbox({
+    this.checkboxState,
+    this.toggleCheckboxState,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      activeColor: Colors.lightBlueAccent,
+      // we have to manage the state of a checkbox
+      // unlike what we're used to in the web world
+      onChanged: toggleCheckboxState,
     );
   }
 }
